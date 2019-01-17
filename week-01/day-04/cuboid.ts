401621f5d7b1eb4abee628b1ec41fadabd8a3744
Adam HOMEPC <adam_nagy89@yahoo.com>
2019-01-17 21:48:04 +0100

    Finished cuboid.ts code, with number type variables as we did not cover floats yet.

--- week-01/day-04/cuboid.ts
+++ week-01/day-04/cuboid.ts
@@ -1,3 +1,11 @@
 'use strict';
 
+let a: number = 10;
+let b: number = 15;
+let c: number = 7;
 
+let surfaceArea: number = (a * b + a *c + b * c) * 2;
+let volume: number = a * b * c;
+
+console.log('Surface area: ' + surfaceArea);
+console.log('Volume: ' + volume);
