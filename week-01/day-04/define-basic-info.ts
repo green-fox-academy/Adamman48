5fa3a92f4c9b2822ae93f207ed4b106165b399b7
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-01-17 16:17:02 +0100

    Added some basic information about myself in define-basic-info.ts.

---
+++ week-01/day-04/define-basic-info.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+let myName: string = 'Adam Pisky-Nagy';
+let myAge: number = 29;
+let myHeight: number = 1.87;
+let relationshipStatus: boolean = false;
+
+console.log('My name: ' + myName);
+console.log('My age: ' + myAge);
+console.log('My height: ' + myHeight);
+console.log('In a relationship: ' + relationshipStatus);
