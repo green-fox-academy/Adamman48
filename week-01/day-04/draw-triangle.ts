fc3c49b832021daf99064eb96522d407add6aa96
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-01-18 14:45:21 +0100

    Corrected the program in fizz-buzz.ts. Had to swap two lines to get the exact expected outcome (if and second else if instructions).

---
+++ week-01/day-04/draw-triangle.ts
@@ -0,0 +1,5 @@
+'use.strict';
+
+let lineCount: number = 4;
+
+for (let i: number = 1)
--- week-01/day-04/fizz-buzz.ts
+++ week-01/day-04/fizz-buzz.ts
@@ -1,12 +1,12 @@
 'use.strict';
 
 for (let i: number = 1; i <= 100; i++) {
-    if (i % 3 === 0) {
-        console.log ('Fizz');
+    if (i % 3 === 0 && i % 5 ===0) {
+        console.log('Fizz Buzz');
     } else if (i % 5 === 0) {
         console.log('Buzz');
-    } else if (i % 3 === 0 && i % 5 ===0) {
-        console.log('Fizz Buzz');
+    } else if (i % 3 === 0) {
+        console.log ('Fizz');
     } else {
         console.log(i);
     }
