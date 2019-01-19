3bcfe7e7e9c18b3dbee1fe33b68ce2249dca5002
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-01-19 23:34:11 +0100

    Finished the pyramid drawing program in draw-pyramid.ts. The size of the pyramid is flexible.

--- week-01/day-04/draw-pyramid.ts
+++ week-01/day-04/draw-pyramid.ts
@@ -2,15 +2,15 @@
 
 export {}
 
-let lineCount: number = 4;
-let star: any = '*';
+let lineCount: number = 10;
+let star: any = '';
 let space: any = '';
 
 for (let i: number = 0; i < lineCount; i++) {
     for (let sp: number = 0; sp < lineCount - i; sp++) {
-    space += '0';
+    space += ' ';
     }
-    for (let st: number = 1; st < i; st++) {
+    for (let st: number = 0; st < i * 2 + 1; st++) {
     star += '*'
     }
 console.log(space + star);
