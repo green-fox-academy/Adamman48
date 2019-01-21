cb99033f5b0b1afb4fdc86abc1be3d13722a1293
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-01-21 16:26:23 +0100

    Created a function which checks which one has more elements out of two arrays in compare-length.ts

---
+++ week-02/day-01/compare-length.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+export {};
+
+let firstList: number[] = [1, 2, 3];
+let secondList: number[] = [4, 5];
+
+if (firstList.length < secondList.length) {
+    console.log(true);
+} else {
+    console.log(false);
+}
