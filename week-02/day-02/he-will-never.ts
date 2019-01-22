a89ab69588587cecb8e2664818fcddbbc9d8fb4b
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-01-22 16:43:02 +0100

    Found the solution for an excercise where I had to use an object's elements combined with an array's elements, with the combination of .map/.join. In he-will-never.ts.

---
+++ week-02/day-02/he-will-never.ts
@@ -0,0 +1,23 @@
+'use strict';
+
+export{};
+
+let out: string = '';
+let notSoCrypticMessage: number[] = [1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11];
+
+
+let hashmap = {
+    7: 'run around and desert you',
+    50: 'tell a lie and hurt you ',
+    49: 'make you cry, ',
+    2: 'let you down',
+    12: 'give you up, ',
+    1: 'Never gonna ',
+    11: '\n',
+    3: 'say goodbye '
+};
+
+out = notSoCrypticMessage.map(e => 
+    hashmap[e]).join('');
+
+    console.log(out);  
