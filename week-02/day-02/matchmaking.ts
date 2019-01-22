dbd85ee9f707680416a6ab3732a612af3930a256
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-01-22 17:46:31 +0100

    Created the file matchmaking.ts where I am going to write a function-based code.

---
+++ week-02/day-02/matchmaking.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+export{};
+
+// Write a function that joins two array by matching one girl with one boy in a new array
+// If someone has no pair, he/she should be the element of the array too
+// Exepected output: ["Eve", "Joe", "Ashley", "Fred"...]
+
+let girls: string[] = ['Eve', 'Ashley', 'Claire', 'Kat', 'Jane'];
+let boys: string[] = ['Joe', 'Fred', 'Tom', 'Todd', 'Neef', 'Jeff'];
+
+
+
+console.log(makingMatches(girls, boys));
+
+export = makingMatches;
