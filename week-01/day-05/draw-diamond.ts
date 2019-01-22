d5a941b0a9fab32f7c72ca0d8ffe12d7e580aa0a
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-01-22 09:45:59 +0100

    Updated draw-diamond.ts in week-01/day-05.

--- week-01/day-05/draw-diamond.ts
+++ week-01/day-05/draw-diamond.ts
@@ -8,7 +8,7 @@ let space: any = '';
 
 for (let i: number = 0; i < lineCount; i++) {
     for (let sp1: number = 0; sp1 < lineCount - i; sp1++) {
-    space += '0';
+    space += ' ';
     }
     for (let st1: number = 0; st1 < i * 2 + 1; st1++) {
     star += '*'
@@ -19,10 +19,10 @@ star = '';
 }
 
 for (let k: number = 0; k < lineCount; k++) {
-    for (let sp2: number = 1; sp2 < k + lineCount / 2; sp2++) {
-    space += '0';
+    for (let sp2: number = 0; sp2 < k + lineCount / 2; sp2++) {
+    space += ' ';
     }
-    for (let st2: number = 0; st2 ;st2++) {
+    for (let st2: number = lineCount; st2 > lineCount - 1; st2--) {
     star += '*';
     }
 console.log(space + star);
@@ -30,3 +30,20 @@ space = '';
 star = '';
 }
 
+0  5  0 2*4+1 
+1  5  0 2*4+1
+2  5  0 
+3  5  0
+4  5  0
+
+5  5  0
+
+0  3  1
+1  3  1
+2  3  1
+
+3  3  1
+
+0  1  2
+
+1  1  2
