8d158f9add067a305c2206bc59413732f61d8c15
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-01-22 22:57:22 +0100

    Created a code with a function which appends the letter 'a' at the end of an array's elements, in append-letters.ts

---
+++ week-02/day-02/append-letter.ts
@@ -0,0 +1,25 @@
+'use strict';
+
+export{};
+
+// Create a function called "appendA()" that adds "a" to every string in the far list.
+// The parameter should be a list.
+// The output should be: 'boa', 'anaconda', 'koala', 'panda', 'zebra'
+
+let far: string[] = ['bo', 'anacond', 'koal', 'pand', 'zebr'];
+
+function appendA(a: string[]) {
+    for (let i: number = 0; i < a.length; i++) {
+        a[i] = a[i] + 'a';
+    }
+    return a;
+}
+
+/*
+function appendA(e =>
+    a = a.mape.concat('a')
+    )
+*/
+console.log(appendA(far));
+
+export = appendA;
