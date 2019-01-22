c10af0e8a0548b69c46aa0ea3d3e4da155ae7ce5
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-01-22 13:28:25 +0100

    Practiced string replacement in simple-replace.ts.

---
+++ week-02/day-02/simple-replace.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+export{};
+
+let example: string = 'In a dishwasher, far far away...';
+
+example = example.replace('dishwasher', 'galaxy');
+
+console.log(example);
+
+export = example;
