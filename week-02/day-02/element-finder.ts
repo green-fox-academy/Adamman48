484cba0599254529b1265fdc304a6aba7cd47631
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-01-23 00:17:51 +0100

    Added two more versions of code with functions (while loop and if statement) to search for the given array's certain element, returning a string depending on the outcome, whithin element-finder.ts.

--- week-02/day-02/element-finder.ts
+++ week-02/day-02/element-finder.ts
@@ -10,6 +10,7 @@ export{};
 
 const numbers: number[] = [1, 2, 3, 4, 5, 6, 8];
 
+/* VERSION 1 (for loop)
 function containsSeven(a: number[]) {
     let answer: string = ''
     for (let i: number = 0; i < a.length; i++) {
@@ -21,7 +22,30 @@ function containsSeven(a: number[]) {
     }
     return answer;
 }
+*/
+// VERSION 2 (if statement)
+function containsSeven(a: number[]) {
+    let answer: string = '';
+    if (a.indexOf(7) <= 0) {
+        answer = 'Noooooo';
+    } else {
+        answer = 'Hoorray';
+    }
+    return answer;
+}
 
+/* VERSION 3 (while loop)
+function containsSeven(a: number[]) {
+    let answer: string = ''
+    while (a.indexOf(7) >= 0) {
+        answer = 'Hoorray';
+    } while (a.indexOf(7) < 0) {
+        answer = 'Nooooo';
+        break;
+    }
+    return answer;
+}
+*/
 console.log(containsSeven(numbers));
 
 export = containsSeven;
