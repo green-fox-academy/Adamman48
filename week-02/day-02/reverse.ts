7ca0531cb4db42e7a8f38890dff9f0aacc1077e8
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-01-22 13:44:33 +0100

    Reversed a string/sentence with split/reverse/join method in reverse.ts.

---
+++ week-02/day-02/reverse.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+export{};
+
+let reversed: string = '.eslaf eb t\'nduow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI';
+
+reversed = reversed.split('').reverse().join('');
+
+console.log(reversed);
+
+export = reversed;
