b42e7ca884b636c89247a486944b4d4be02674b9
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-01-22 21:52:04 +0100

    Created a function which merges two arrays with different element count into one, with a[i] and b [i] next to eachother, in matchmaking.ts.

--- week-02/day-02/matchmaking.ts
+++ week-02/day-02/matchmaking.ts
@@ -9,7 +9,12 @@ export{};
 let girls: string[] = ['Eve', 'Ashley', 'Claire', 'Kat', 'Jane'];
 let boys: string[] = ['Joe', 'Fred', 'Tom', 'Todd', 'Neef', 'Jeff'];
 
-
+function makingMatches(a: string[], b: string[]) {
+    let matches: string[] = [];
+    for (let i: number = 0; i <= a.length && i <= b.length; i++) {
+        matches = matches.concat(a.slice(i, i+1).concat(b.slice(i, i+1)));
+        } return matches;
+}
 
 console.log(makingMatches(girls, boys));
 
