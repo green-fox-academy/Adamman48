860f3167d81b6ce67e8abc5d41c93d3890a251be
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-01-24 11:53:20 +0100

    Created a function which draws lines from the y to the x axis and vice versa, with different colors each of the two formations. The function only needs one input, the scale of the formation. line-play.ts

---
+++ week-02/day-04/line-play.ts
@@ -0,0 +1,27 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+let scale: number = 8;
+
+function drawLines(x) {
+    let kolor: string[] = ['green', 'purple'];
+    for (let i: number = x; i < canvas.width; i = i + x) {
+        if (x <= 0) {
+        x = 1;
+    }
+        ctx.strokeStyle = kolor[0];
+        ctx.beginPath();
+        ctx.moveTo((canvas.width - canvas.width), i);
+        ctx.lineTo(i, canvas.height);
+        ctx.stroke();
+            ctx.strokeStyle = kolor[1];
+            ctx.beginPath();
+            ctx.moveTo(canvas.width, (canvas.height - i));
+            ctx.lineTo((canvas.width - i), (canvas.height - canvas.height));
+            ctx.stroke();
+    }
+}
+
+drawLines(scale);
