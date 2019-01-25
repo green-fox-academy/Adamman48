5a43eda2496c69b59f8358de5f64f2a8e210e4d4
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-01-25 11:04:47 +0100

    Created a function that draws squares with the input of the two starting coordinates and the size of the square. Trying to create a checkerboard and working on the filling colors.

---
+++ week-02/day-05/checkerboard.ts
@@ -0,0 +1,34 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Fill the canvas with a checkerboard pattern.
+
+function drawSquares(startX: number, startY: number, size: number) {
+    ctx.fillRect(startX, startY, size, size);
+}
+
+let startCoordX: number = 1;
+let startCoordY: number = 1;
+let sizeOf: number = 50;
+let rowsColumns: number = 8;
+let kolor1: string = 'white';
+let kolor2: string = 'black';
+
+for (let i: number = 0; i < rowsColumns; i++) {
+    let moveX = i;
+    for (let j: number = 0; j < rowsColumns; j++) {
+        if (j % 2 === 0) {
+            ctx.fillStyle = kolor1;
+        } else {
+            ctx.fillStyle = kolor2;
+        }
+            drawSquares(startCoordX, startCoordY, sizeOf);
+        startCoordY = startCoordY + sizeOf;
+    }
+    startCoordX = startCoordX + sizeOf;
+    startCoordY = (startCoordY - startCoordY) + 1;
+}
