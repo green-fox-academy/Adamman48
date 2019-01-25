8994943cd9c2f7601b6c493453c9ba22668fb23a
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-01-25 11:34:55 +0100

    Finished the checkerboard drawing in checkerboard.ts, which utilizes a square drawing function in a for loop. The size, and the number of columns/rows are adjustable, as well as the two colors of the boar.

--- week-02/day-05/checkerboard.ts
+++ week-02/day-05/checkerboard.ts
@@ -8,22 +8,29 @@ const ctx = canvas.getContext('2d');
 // Fill the canvas with a checkerboard pattern.
 
 function drawSquares(startX: number, startY: number, size: number) {
-    ctx.strokeRect(startX, startY, size, size);
+    ctx.strokeStyle = 'black';
+    ctx.fillRect(startX, startY, size, size);
+    ctx.strokeRect( startX, startY, size, size);
 }
 
 let startCoordX: number = 1;
 let startCoordY: number = 1;
-let sizeOf: number = 50;
+let sizeOf: number = 40;
 let rowsColumns: number = 8;
 let kolor1: string = 'white';
 let kolor2: string = 'black';
 
 for (let i: number = 0; i < rowsColumns; i++) {
-    for (let j: number = 0; j < rowsColumns; j++) {
-            ctx.strokeStyle = kolor2;
+    for (let j: number = i; j < rowsColumns + i; j++) {
+        if (j % 2 === 0) {
+            ctx.fillStyle = kolor1;
+        } else {
+            ctx.fillStyle = kolor2
+        }
             drawSquares(startCoordX, startCoordY, sizeOf);
         startCoordY = startCoordY + sizeOf;
-        }
+        ctx.fillStyle = kolor2;
+    }
     startCoordX = startCoordX + sizeOf;
     startCoordY = (startCoordY - startCoordY) + 1;
 }
