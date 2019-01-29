3654ec980177f60ed3f01a62f04c1342c4ca52f8
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-01-29 15:02:59 +0100

    Created write-single-line.ts, which contains a function that writes an input name to an input file. If the file already exists and has content, it adds the name to a new line. If it is unable to write, it gives a custom error message.

---
+++ week-03/day-02/write-single-line.ts
@@ -0,0 +1,32 @@
+'use strict';
+
+export{};
+
+// Write a function that is able to manipulate a file
+// By writing your name into it as a single line
+// The file should be named "my-file.txt"
+// In case the program is unable to write the file,
+// It should print the following error message: "Unable to write file: my-file.txt"
+
+const fs = require('fs');
+let inputName: string = 'Adam Pisky-Nagy';
+let inputFile: string = 'my-file.txt'
+
+function writeToMyFile(fileName: string, nameToWrite: string) {
+    let emptyFile: boolean = fileName.split('').length <= 0;
+    let notEmptyFile: boolean = fileName.split('').length > 0;
+    let noFile: boolean = fs.existsSync(fileName) === false;
+    try { 
+        if (noFile || emptyFile) {
+            fs.appendFileSync(fileName, nameToWrite);
+        }
+        else if (notEmptyFile) {
+        fs.appendFileSync(fileName, '\n' + nameToWrite);
+        }
+    }
+    catch {
+        console.log('Unable to write file: ' + fileName);
+    }
+}
+
+writeToMyFile(inputFile, inputName);
