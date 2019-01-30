b073485d12712fa947d1ec0d4dcb2802610d3f52
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-01-30 13:53:40 +0100

    Created animals.ts which includes a class for animals, with two properties(given default values) and 3 functions, which influences the properties.

---
+++ week-03/day-03/animal.ts
@@ -0,0 +1,37 @@
+'use strict';
+
+/*Create an Animal class
+Every animal has a hunger value, which is a whole number
+Every animal has a thirst value, which is a whole number
+when creating a new animal object these values are created with the default 50 value
+Every animal can eat() which decreases their hunger by one
+Every animal can drink() which decreases their thirst by one
+Every animal can play() which increases both by one*/
+
+class Animal {
+    hunger: number;
+    thirst: number;
+
+    constructor (h: number = 50, t: number = 50) {
+        this.hunger = h;
+        this.thirst = t;
+    }
+
+    eat() {
+        this.hunger--;
+    }
+    drink() {
+        this.thirst--;
+    }
+    play() {
+        this.hunger++;
+        this.thirst++;
+    }
+}
+
+let lion: Animal = new Animal();
+let cassuary: Animal = new Animal();
+let tardigrade: Animal = new Animal();
+
+lion.play();
+console.log(lion.hunger);
