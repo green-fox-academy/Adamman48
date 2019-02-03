afcea4c2d7fe702cddb8e9a68a811fde347f9180
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-02-03 14:17:32 +0100

    Created teachers-and-students.ts in week-03/day-05, which contains two classes and linked functions between them.

--- week-03/day-05/teachers-and-students.ts
+++ week-03/day-05/teachers-and-students.ts
@@ -9,9 +9,44 @@ teach(student) -> calls the students learn method
 answer()*/
 
 class Student {
+    name: string;
+    knowledge: number;
 
+    constructor (inputName: string) {
+        this.name = inputName;
+        this.knowledge = 50;
+    }
+    
+    learn() {
+        this.knowledge += 5;
+    }
+    question(inputTeacher: Teacher) {
+        inputTeacher.answer();
+    }
 }
 
 class Teacher {
-    teach
+    name: string;
+    patience: number;
+
+    constructor (inputName: string) {
+        this.name = inputName;
+        this.patience = 50;
+    }
+
+    teach(inputStudent: Student) {
+        inputStudent.learn();
+    }
+    answer() {
+        this.patience -= 5;
+    }
 }
+
+let Adam: Student = new Student('Adam');
+let Mrs_Holloway: Teacher = new Teacher('Mrs. Holloway');
+
+Adam.question(Mrs_Holloway);
+Mrs_Holloway.teach(Adam);
+
+console.log(Mrs_Holloway)
+console.log(Adam);
