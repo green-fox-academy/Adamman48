a45065c371d3b0866b2c9daa41be8595b5b06e9e
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-02-06 22:18:05 +0100

    Created the spine for zoo.ts task's code. Off to sleep...

---
+++ week-04/day-03/animals_in_the_zoo/zoo.ts
@@ -0,0 +1,29 @@
+'use strict';
+
+abstract class Animal {
+    name: string;
+    age: number;
+    sex: string;
+    happiness: boolean;
+    hunger: boolean;
+
+    getName(): void {
+
+    };
+
+    breed(): void {
+
+    };
+
+    feed(): void {
+
+    };
+
+    setFree(): void {
+
+    };
+
+    encage(): void {
+
+    };
+}
