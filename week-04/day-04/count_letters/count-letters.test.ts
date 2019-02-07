957aefc402d5249e94ef2deb18868226d21a0f76
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-02-07 23:28:31 +0100

    Minor cosmetic change to test function.

--- week-04/day-04/count_letters/count-letters.test.ts
+++ week-04/day-04/count_letters/count-letters.test.ts
@@ -7,6 +7,7 @@ test('counting letters and filtering out all other kinds of characters', t => {
     let actual: number = Object.keys(letterCounter(testString))  //Object.values() doesn't seem to work.
     .map(key => letterCounter(testString)[key])                  //Creating an array of object keys and matching it with object's values.
     .reduce((prevValue, currentValue) => {return prevValue + currentValue}, 0);
+    
     let expected: number = 12;
 
     t.equal(actual, expected);
