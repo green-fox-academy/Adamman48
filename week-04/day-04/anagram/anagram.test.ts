1e49a524aebf312bdbfcbf9a1695cfcb50f94f88
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-02-07 15:08:46 +0100

    Finished anagramChecker's test function inside anagram folder. Tester works properly.

--- week-04/day-04/anagram/anagram.test.ts
+++ week-04/day-04/anagram/anagram.test.ts
@@ -5,6 +5,6 @@ test('check if two string arrays are the exact same after sorting', t => {
     let actual: boolean = anagramChecker('adam', 'maad');
     let expected: boolean = true;
 
-    t.equal()
-
-})
+    t.equal(actual, expected);
+    t.end();
+});
