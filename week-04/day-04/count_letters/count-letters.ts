9ce756bc1bc21584ed6cc6cfd956064e7f55027c
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-02-07 17:37:00 +0100

    Finished letter-counter task's main function which gives me an object, unlike the previous state which returned an array filled with objects. Whitespace filtering is present, but need to add special character filtering as well. Want to use function inside replace method.

--- week-04/day-04/count_letters/count-letters.ts
+++ week-04/day-04/count_letters/count-letters.ts
@@ -1,2 +1,17 @@
 'use strict';
 
+/* Write a function, that takes a string as an argument and returns a dictionary with all letters in the string as keys, and numbers as values that shows how many occurrences there are.
+Create a test for that. */
+
+function letterCounter(inputString: string) {
+    let dictionary: {[letter: string]: number} = {};
+
+    inputString.replace(/\s/g, '').split('')
+    .forEach((value, index, array) => dictionary[value] = array.filter(v => v === value).length);
+
+    return dictionary;
+}
+
+console.log(letterCounter('adam'))
+
+export {letterCounter};
