308c5e57ac1cdcd73d947e347a4cf3483f56b001
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-02-23 13:25:37 +0100

    Practicing with revisiting diamond drawing in command line. Planning to use nested for loops to achieve it.

---
+++ week-01/day-04/draw-diamond.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+/* Write a program that draws a
+// diamond like this:
+//
+//    *   
+//   ***
+//  *****
+// *******
+//  *****
+//   ***
+//    *
+//
+// The diamond should have as many lines as lineCount is */
+
+let lineCount: number = 7;
+
+for (let i: number = 1; i < lineCount * 2; i += 2) {
+    for (let j: number = 0; j < lineCount; j++) {
+      
+    }
+}
