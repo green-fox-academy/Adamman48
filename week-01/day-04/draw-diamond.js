8c83dacdb3052eb3c6e7b22ce8f91563b209e447
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-02-23 14:37:15 +0100

    Diamond drawing is almost done but only works with odd numbers given in lineCount. Need to figure it out.

--- week-01/day-04/draw-diamond.js
+++ week-01/day-04/draw-diamond.js
@@ -13,14 +13,16 @@
 //
 // The diamond should have as many lines as lineCount is */
 
-let lineCount = 7;
+let lineCount = 8;
 let spacing = Math.floor(lineCount / 2);
+let starCountAfterTurn = lineCount - 2;
 
 for (let i = 1; i < lineCount * 2; i += 2) {
   let whitespace = ' ';
   let star = '*';
 
-  console.log(whitespace.repeat(spacing).concat(star.repeat(i)));
+  console.log(whitespace.repeat(spacing).concat(star.repeat(i > lineCount ? starCountAfterTurn : i)));
 
   i < lineCount ? spacing-- : spacing++;
+  i > lineCount ? starCountAfterTurn -= 2 : null;
 }
