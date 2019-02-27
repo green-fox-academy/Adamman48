bbff33279be16d80b301cca0c93f8c9a6d3c4f31
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-02-27 11:29:25 +0100

    Initiated the first excercise with DOM, using a ts file and then compiling it to js.

---
+++ Orientation Phase/week-06/day-03/queryselector-1.ts
@@ -0,0 +1,14 @@
+'use strict';
+
+/* 1. store the element that says 'The King' in the 'king' variable.
+console.log it.
+2. store 'The Businessman' and 'The Lamplighter'
+in the 'businessLamp' variable.
+console.log each of them.
+3. store 'The King' and 'The Conceited Man'
+in the 'conceitedKing' variable.
+alert them one by one.
+4. store 'The King', 'The Conceited Man' and 'The Lamplighter'
+in the 'noBusiness' variable.
+console.log each of them. */
+
