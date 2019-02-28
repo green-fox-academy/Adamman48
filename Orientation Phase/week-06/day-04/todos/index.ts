549b9f334889e0e7642598def6d5c710183ea20a
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-02-28 18:11:10 +0100

    Trying out a solution to todos with combining DOM and ejs.

--- Orientation Phase/week-06/day-04/todos/index.ts
+++ Orientation Phase/week-06/day-04/todos/index.ts
@@ -11,6 +11,22 @@ app.listen(PORT, () => {
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
+const todos: string[] = [
+  'buy Cocaine...on iTunes',
+  'listen to Cocaine',
+  'bed',
+];
+
+function addList(inputArray: string[]) {
+  let listParentNode: HTMLElement = document.querySelector('ul');
   
+  for (let i: number = 0; i < inputArray.length; i++) {
+    let newListElement: HTMLElement = document.createElement('li');
+    newListElement.textContent = `${inputArray[i]}`;
+    listParentNode.appendChild(newListElement);
+  }
+}
+
+app.get('/', (req, res) => {
+  res.render('home', addList(todos));
 })
