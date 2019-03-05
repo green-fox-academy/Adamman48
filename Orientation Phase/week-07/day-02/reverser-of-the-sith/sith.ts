499b0f77b007347089d02b45e5891c5caf56194c
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-03-06 00:14:10 +0100

    Work in progress for sith task. Working on a way to repeat the proccess.

--- Orientation Phase/week-07/day-02/reverser-of-the-sith/sith.ts
+++ Orientation Phase/week-07/day-02/reverser-of-the-sith/sith.ts
@@ -8,8 +8,18 @@ app.use(express.json());
 
 app.post('sith', (req, res) => {
   let input: string = req.body.text;
+  let startPoint: number = 0;
+  let endPoint: number = input.indexOf('\s' || '\W', startPoint);
+  let transformArray: string[] = [];
+  let output: {[sith_text: string]: string} = {};
 
-  
+  transformArray.push(input.slice(startPoint, endPoint).toLowerCase());
+  startPoint = endPoint;
+  transformArray.unshift(input.slice(startPoint, endPoint).toLowerCase());
+  startPoint = endPoint;
+
+  input.indexOf('\W')
+
 })
 
 app.listen(PORT, () => {
