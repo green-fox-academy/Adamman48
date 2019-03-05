1d85866cbe03f2cf9bffc9ab20679ddd8ca5f2b2
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-03-05 14:32:54 +0100

    Solved the doubling excercise.

--- Orientation Phase/week-07/day-02/app.js
+++ Orientation Phase/week-07/day-02/app.js
@@ -1,11 +1,24 @@
 'use strict';
 var express = require('express');
 var app = express();
-var PORT = 3000;
+var PORT = 8080;
 var path = require('path');
 app.use('/assets', express.static('assets'));
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+app.get('/doubling', function (req, res) {
+    var inputNumber = req.query.input;
+    var output = {};
+    isNaN(inputNumber) ?
+        output = {
+            error: "Please provide an input!"
+        } :
+        output = {
+            received: Number(inputNumber),
+            result: Number(inputNumber * 2)
+        };
+    res.send(output);
 });
 app.listen(PORT, function () {
     console.log("I'm running!");
--- Orientation Phase/week-07/day-02/app.ts
+++ Orientation Phase/week-07/day-02/app.ts
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 const path = require('path');
 
 app.use('/assets', express.static('assets'));
@@ -10,6 +10,22 @@ app.use('/assets', express.static('assets'));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+app.get('/doubling', (req, res) => {
+  let inputNumber: number = req.query.input;
+  let output: Object = {};
+
+  isNaN(inputNumber) ?
+    output = {
+      error: "Please provide an input!"
+    } :
+    output = {
+      received: Number(inputNumber),
+      result: Number(inputNumber * 2),
+    };
+  
+  res.send(output);
+})
 
 app.listen(PORT, () => {
   console.log(`I'm running!`);
