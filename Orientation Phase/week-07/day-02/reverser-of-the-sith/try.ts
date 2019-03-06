d0f95289cd59d83d3c7c69b8dd0783cc33284272
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-03-06 11:21:53 +0100

    I have an array with arrays of every input sentences' words as elements inside them, all in lower case, without non-word characters. Next step is to determine whether the sentences are odd-numbered or not. Then I will rearrange the elements (words) accordingly.

--- Orientation Phase/week-07/day-02/reverser-of-the-sith/try.ts
+++ Orientation Phase/week-07/day-02/reverser-of-the-sith/try.ts
@@ -1,15 +1,18 @@
 'use strict';
 
   let input: string = `This is a try? I don't know! What the fuck is going on. Believe me.`;
-  let fillThis = [];
-  let output = [];
   let tempArray = [];
-
-  fillThis = input.toLowerCase().split('?').join('$').split('.').join('$').split('!').join('$').split('$')
-
-  fillThis.splice(0, fillThis.length - 1).forEach(value => tempArray.push(value.split(/\s/)));
-
+  let output = [];
   
+  let fillThis: string[] = input.toLowerCase()
+    .split('?').join('$')
+      .split('.').join('$')
+        .split('!').join('$').split('$');
 
+  fillThis.splice(0, fillThis.length - 1)
+    .forEach(value => tempArray.push(value.split(/\s/)));
 
+  tempArray.map((value, index) => 
+    {index > 0 ? value.splice(0, 1) : null});
+  
   console.log(tempArray)
