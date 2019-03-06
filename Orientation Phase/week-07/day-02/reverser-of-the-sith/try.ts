d8e8cd6e123d4cd8579a87b4a22b158163969128
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-03-06 11:55:17 +0100

    Reversed the sentences' words in the arrays inside the array. Now I will join them but leave them inside the array. I'll have to determine the end of sentences and uppercase the first letter in each sentence.

--- Orientation Phase/week-07/day-02/reverser-of-the-sith/try.ts
+++ Orientation Phase/week-07/day-02/reverser-of-the-sith/try.ts
@@ -1,18 +1,24 @@
 'use strict';
 
-  let input: string = `This is a try? I don't know! What the fuck is going on. Believe me.`;
-  let tempArray = [];
-  let output = [];
+let input: string = `This is a try? I don't know! What the fuck is going on. Believe me.`;
+let tempArray = [];
+let output = [];
   
-  let fillThis: string[] = input.toLowerCase()
-    .split('?').join('$')
-      .split('.').join('$')
-        .split('!').join('$').split('$');
+let fillThis: string[] = input.toLowerCase()
+  .split('?').join('$')
+    .split('.').join('$')
+      .split('!').join('$').split('$');
 
-  fillThis.splice(0, fillThis.length - 1)
-    .forEach(value => tempArray.push(value.split(/\s/)));
+fillThis.splice(0, fillThis.length - 1)
+  .forEach(value => tempArray.push(value.split(/\s/)));
 
-  tempArray.map((value, index) => 
-    {index > 0 ? value.splice(0, 1) : null});
+tempArray.map((value, index) => 
+  {index > 0 ? value.splice(0, 1) : null});
   
-  console.log(tempArray)
+for (let i: number = 0; i < tempArray.length; i++) {
+  for (let j: number = 1; j < tempArray[i].length; j += 2) { 
+    [tempArray[i][j-1], tempArray[i][j]] = [tempArray[i][j], tempArray[i][j-1]];
+  }
+}  
+
+console.log(tempArray)
