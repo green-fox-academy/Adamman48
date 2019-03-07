265d06f5505263feb03cbb4ef89e053bc4dd94a3
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-03-07 11:54:58 +0100

    Created the database bookinfo in mysql shell. Now setting up the querys in the server file.

--- Orientation Phase/week-07/day-04/mysqlNode/server.js
+++ Orientation Phase/week-07/day-04/mysqlNode/server.js
@@ -1,13 +1,16 @@
 'use strict';
+require('dotenv').config();
 var express = require('express');
 var app = express();
 var PORT = 3000;
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
+app.use(express.json());
 /* connection.connect((err) => {
   err ? console.error('Error connecting to DB') :
     console.log('DB is connected');
@@ -17,7 +20,8 @@ app.get('/', function (req, res) {
     connection.query('SHOW DATABASES;', function (err, rows) {
         err ? (console.log(err),
             res.status(500).send()) :
-            res.send(rows);
+            null;
+        res.send(rows);
     });
 });
 app.listen(PORT, function () {
--- Orientation Phase/week-07/day-04/mysqlNode/server.ts
+++ Orientation Phase/week-07/day-04/mysqlNode/server.ts
@@ -1,15 +1,19 @@
 'use strict';
 
+require('dotenv').config();
 const express = require ('express');
 const app = express();
 const PORT = 3000;
 const mysql = require ('mysql');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 });
+
+app.use(express.json());
 
 /* connection.connect((err) => {
   err ? console.error('Error connecting to DB') :
