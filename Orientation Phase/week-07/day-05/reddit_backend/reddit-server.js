09591c67269c39a96a07471d48b685cc54df6e07
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-03-08 15:15:21 +0100

    Created the /posts get request endpoint with header setting and checking.

--- Orientation Phase/week-07/day-05/reddit_backend/reddit-server.js
+++ Orientation Phase/week-07/day-05/reddit_backend/reddit-server.js
@@ -6,6 +6,12 @@ var app = express();
 var path = require('path');
 var mysql = require('mysql');
 var konzola = console.log;
+var reddit = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_db
+});
 var errorHandling = function (response, error) {
     error ? (konzola(error),
         response.status(500).send()) :
@@ -13,8 +19,23 @@ var errorHandling = function (response, error) {
 };
 app.use('/assets', express.static('assets'));
 app.use(express.json());
+reddit.connect(function (error) {
+    error ? konzola('The cookie jar is not reachable. :(') :
+        konzola('The cookie jar is in your hands. Feast!');
+});
 app.get('/hello', function (req, res) {
     res.send('Hello Boss!');
+});
+app.get('/posts', function (req, res) {
+    res.set('Content-type', 'application/json');
+    req.get('Content-type') === 'application/json' ?
+        reddit.query("SELECT * FROM posts;", function (error, rows) {
+            errorHandling(res, error);
+            res.json(rows);
+        }) : res.send('Request is invalid. You loser!');
+});
+app.post('/posts', function (req, res) {
+    res.set();
 });
 app.listen(PORT, function () {
     konzola('What is thy bidding, my master?');
--- Orientation Phase/week-07/day-05/reddit_backend/reddit-server.ts
+++ Orientation Phase/week-07/day-05/reddit_backend/reddit-server.ts
@@ -8,6 +8,13 @@ const path = require ('path');
 const mysql = require ('mysql');
 const konzola = console.log;
 
+const reddit = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PW,
+  database: process.env.DB_db
+});
+
 const errorHandling = (response, error) => {
   error ? (konzola(error),
       response.status(500).send()) :
@@ -17,11 +24,27 @@ const errorHandling = (response, error) => {
 app.use('/assets', express.static('assets'));
 app.use(express.json());
 
+reddit.connect((error) => {
+  error ? konzola('The cookie jar is not reachable. :(') :
+    konzola('The cookie jar is in your hands. Feast!');
+});
 
 app.get('/hello', (req, res) => {
   res.send('Hello Boss!')
 });
 
+app.get('/posts', (req, res) => {
+  res.set('Content-type', 'application/json');
+  req.get('Content-type') === 'application/json' ?
+    reddit.query(`SELECT * FROM posts;`, (error, rows) => {
+    errorHandling(res, error);
+    res.json(rows);
+  }) : res.send('Request is invalid. You loser!');
+});
+
+app.post('/posts', (req, res) => {
+  res.set()
+})
 
 app.listen(PORT, () => {
   konzola('What is thy bidding, my master?');
