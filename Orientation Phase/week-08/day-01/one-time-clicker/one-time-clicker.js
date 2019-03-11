ad2c1ae10a749e42afab0b28e7fed4d3de49249a
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-03-11 12:15:09 +0100

    Solved the one-time clicker task three different ways. First I use the in-built disable attribute of the button tag, then I use a variabble to check if the button has already been called, and third I use getelementbyId to target the button and change the id at the end of the event so it can't be called again.

--- Orientation Phase/week-08/day-01/one-time-clicker/one-time-clicker.js
+++ Orientation Phase/week-08/day-01/one-time-clicker/one-time-clicker.js
@@ -1,25 +1,21 @@
 'use strict';
-
 /* Create an event listener which logs the current timestamp to the console once the button is clicked.
 When the button is clicked for the second or any other times no event listener should be called.
 Try to solve this two different ways.
 hint: take a look at the button attributes https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button */
-
-let button: HTMLButtonElement = document.querySelector('button');
-let currentTime: number = 0;
-
-button.addEventListener('click', (event): void => {
+var button = document.querySelector('button');
+var currentTime = 0;
+/* button.addEventListener('click', (event): void => {
   console.log(event.timeStamp);
   button.setAttribute('disabled', 'true');
-});
-
+}); */
 /* button.addEventListener('click', (event): void => {
   currentTime === 0 ?
   console.log(event.timeStamp) : null;
   currentTime = event.timeStamp;
 });
-
-document.getElementById('click').addEventListener('click', (event): void => {
-  console.log(event.timeStamp);
-  document.getElementById('click').setAttribute('id', 'unclickable');
-}); */
+ */
+document.getElementById('click').addEventListener('click', function (event) {
+    console.log(event.timeStamp);
+    document.getElementById('click').setAttribute('id', 'unclickable');
+});
