981842b7893c8f5edf7b006a2bb89f873a385825
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-03-11 11:31:55 +0100

    Finished the keyup listener task, using .keycode on the event.

---
+++ Orientation Phase/week-08/day-01/keyup-event/keyup.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+/* 1) Subscibe to keyup events on the global window object
+2) Console log the event object and peek inside
+3) Display the last pressed key's code as "Last pressed key code is: __" */
+
+window.addEventListener('keyup', (event) => {
+  console.log(event.target);
+  document.querySelector('h1').textContent = `Last pressed key code is: ${event.keyCode}`;
+});
