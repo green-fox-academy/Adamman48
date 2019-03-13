d13855707b394275c36228be1c1e5fa60a962bd9
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-03-13 17:29:29 +0100

    Finished and solved the pets task.

--- Orientation Phase/week-08/day-03/button-disable/main.js
+++ Orientation Phase/week-08/day-03/button-disable/main.js
@@ -1,11 +1,20 @@
 'use strict';
-var chimpLove = document.querySelector('.choose-animal');
-var signUp = chimpLove.querySelectorAll('input');
+var signUpActive = document.querySelector('.choose-animal');
+var chimpLove = document.querySelector('.sign-up');
+var chimpUp = chimpLove.querySelectorAll('input');
+var signUp = signUpActive.querySelectorAll('input');
 var baboon = document.querySelector('#baboon');
 var cassuary = document.querySelector('#cassuary');
+var signMeUpScotty = document.querySelector('#yes');
 var chimpBtn = document.querySelector('#chimp-love-in');
+var signBtn = document.querySelector('#sign-up-submit');
 signUp.forEach(function (value) { return value.addEventListener('click', function (event) {
     value === baboon || value === cassuary ?
+        signBtn.removeAttribute('disabled') :
+        signBtn.setAttribute('disabled', 'true');
+}); });
+chimpUp.forEach(function (value) { return value.addEventListener('click', function (event) {
+    value === signMeUpScotty ?
         chimpBtn.removeAttribute('disabled') :
         chimpBtn.setAttribute('disabled', 'true');
 }); });
--- Orientation Phase/week-08/day-03/button-disable/main.ts
+++ Orientation Phase/week-08/day-03/button-disable/main.ts
@@ -1,13 +1,23 @@
 'use strict';
 
-const chimpLove: HTMLElement = document.querySelector('.choose-animal');
-const signUp: any = chimpLove.querySelectorAll('input');
+const signUpActive: HTMLElement = document.querySelector('.choose-animal');
+const chimpLove: HTMLElement = document.querySelector('.sign-up')
+const chimpUp: NodeList = chimpLove.querySelectorAll('input');
+const signUp: NodeList = signUpActive.querySelectorAll('input');
 const baboon: HTMLElement = document.querySelector('#baboon');
 const cassuary: HTMLElement = document.querySelector('#cassuary');
+const signMeUpScotty: HTMLElement = document.querySelector('#yes');
 const chimpBtn: HTMLButtonElement = document.querySelector('#chimp-love-in');
+const signBtn: HTMLButtonElement = document.querySelector('#sign-up-submit');
 
 signUp.forEach(value => value.addEventListener('click', (event) => {
   value === baboon || value === cassuary ?
+    signBtn.removeAttribute('disabled') :
+    signBtn.setAttribute('disabled', 'true');
+}));
+
+chimpUp.forEach(value => value.addEventListener('click', (event) => {
+  value === signMeUpScotty ?
     chimpBtn.removeAttribute('disabled') :
     chimpBtn.setAttribute('disabled', 'true');
 }));
