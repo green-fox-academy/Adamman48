93106e3fd83e0364d575b20f72314d603c0a9361
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-03-13 17:16:25 +0100

    Changed the ternary operator's true condition to remove the disabled attribute from setattribute as it wouldn't truly achieve my goal, only css did. Now it works.

--- Orientation Phase/week-08/day-03/button-disable/main.js
+++ Orientation Phase/week-08/day-03/button-disable/main.js
@@ -1,10 +1,11 @@
 'use strict';
-var signUp = document.querySelectorAll('input');
+var chimpLove = document.querySelector('.choose-animal');
+var signUp = chimpLove.querySelectorAll('input');
 var baboon = document.querySelector('#baboon');
 var cassuary = document.querySelector('#cassuary');
 var chimpBtn = document.querySelector('#chimp-love-in');
 signUp.forEach(function (value) { return value.addEventListener('click', function (event) {
     value === baboon || value === cassuary ?
-        chimpBtn.setAttribute('disabled', 'false') :
+        chimpBtn.removeAttribute('disabled') :
         chimpBtn.setAttribute('disabled', 'true');
 }); });
--- Orientation Phase/week-08/day-03/button-disable/main.ts
+++ Orientation Phase/week-08/day-03/button-disable/main.ts
@@ -1,12 +1,13 @@
 'use strict';
 
-const signUp: NodeList = document.querySelectorAll('input');
+const chimpLove: HTMLElement = document.querySelector('.choose-animal');
+const signUp: any = chimpLove.querySelectorAll('input');
 const baboon: HTMLElement = document.querySelector('#baboon');
 const cassuary: HTMLElement = document.querySelector('#cassuary');
 const chimpBtn: HTMLButtonElement = document.querySelector('#chimp-love-in');
 
 signUp.forEach(value => value.addEventListener('click', (event) => {
   value === baboon || value === cassuary ?
-    chimpBtn.setAttribute('disabled', 'false') :
+    chimpBtn.removeAttribute('disabled') :
     chimpBtn.setAttribute('disabled', 'true');
 }));
